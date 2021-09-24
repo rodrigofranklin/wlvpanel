520ce3ad7cfe3c9c05b64b86b5f3e1dc4ce42b3f
Rodrigo Franklin <rodrigo.franklin.br@gmail.com>
2021-09-24 16:22:18 -0300

    ajustando o restart.

--- server.R
+++ server.R
@@ -1,7 +1,7 @@
 
 server <- function(input, output, session) {
  
-  observeEvent(input$restart,write(NULL, file = "dados/restart.txt"))
+  observeEvent(input$restart,write(NULL, file = "restart.txt"))
   
   output$map <- renderLeaflet({
     # Use leaflet() here, and only include aspects of the map that
