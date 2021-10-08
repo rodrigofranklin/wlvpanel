5d67795a004916cadc47ad4a366668098d3f6b58
Rodrigo Franklin <rodrigo.franklin.br@gmail.com>
2021-10-08 11:33:37 -0300

    Atualização do country_panel:
    - Ordenação dos títulos dos grupos de indicadores;
    
    - Desenho do painel de download.

--- panel_country_all_data.R
+++ panel_country_all_data.R
@@ -1,13 +1,13 @@
 tagList(
-conditionalPanel(
-  "output.pais != ''",
-  
-  # Botão fechar
-  actionButton(
-    inputId = "close_country_data_panel",
-    label = NULL,
-    icon = icon("times"),
-    style ="
+  conditionalPanel(
+    "output.pais != ''",
+    
+    # Botão fechar
+    actionButton(
+      inputId = "close_country_data_panel",
+      label = NULL,
+      icon = icon("times"),
+      style ="
             border-radius: 50%;
             border-color: transparent;
             color: white;
@@ -18,15 +18,15 @@ conditionalPanel(
             z-index: 501;
             background-color: rgba(127,127,127,1);
           "
-  ),
-  
-  # Painel dados país
-  absolutePanel(
-    top = 75,
-    left = 10,
-    height = "calc(100vh - 75px)",
-    width = "calc(100vw - 20px)",
-    style = "
+    ),
+    
+    # Painel dados país
+    absolutePanel(
+      top = 75,
+      left = 10,
+      height = "calc(100vh - 75px)",
+      width = "calc(100vw - 20px)",
+      style = "
             overflow-y:scroll;
             z-index:500;
             padding: 20px;
@@ -35,142 +35,178 @@ conditionalPanel(
             border-width: 1px;
             border-color: rgba(221,221,221,1);
           ",
-    # Título
-    textOutput("pais") %>%
-      div(style = "font-size: 24px; font-weight: bold") ,
-
-    # Panel: country_profile
-    absolutePanel(
-      top = 70,
-      left = 20,
-      style = "width: calc(68% + 20px)",
-      class = "panel panel-default",
+      # Título
+      textOutput("pais") %>%
+        div(style = "font-size: 24px; font-weight: bold") ,
       
-      fluidRow(
-        column(
-          width = 6,
-          textOutput("ano")
-        ),
-        column(
-          width = 6,
-          # chakraSliderInput(
-          #   "ano_painel",
-          #   label = NULL,
-          #   min = ano_min, 
-          #   max = ano_max, 
-          #   sep = "") %>%
-          #   p(style = "font-size: '20px'")
-          
-        )
-      ) %>%
-        div(
-          class = "panel-heading",
-          style = "
+      # Panel: country_profile
+      absolutePanel(
+        top = 70,
+        left = 20,
+        style = "width: calc(68% + 20px)",
+        class = "panel panel-default",
+        
+        fluidRow(
+          column(
+            width = 6,
+            textOutput("ano")
+          ),
+          column(
+            width = 6,
+            # chakraSliderInput(
+            #   "ano_painel",
+            #   label = NULL,
+            #   min = ano_min, 
+            #   max = ano_max, 
+            #   sep = "") %>%
+            #   p(style = "font-size: '20px'")
+            
+          )
+        ) %>%
+          div(
+            class = "panel-heading",
+            style = "
                 background-image:none;
                 background: white;
                 font-size: 16px; 
                 font-weight: bold;
                 padding: 3px 5px;
               "
-        ),
-      
-      dataTableOutput("profile") %>%
-        div(
-          class = "panel-body",
-          style = "
+          ),
+        
+        dataTableOutput("profile") %>%
+          div(
+            class = "panel-body",
+            style = "
                   background-image:none;
                   background: white;
                   padding: 0px;
                 "
-        )
-    ),
-    
-    # Painel de download
-    absolutePanel(
-      top = 70,
-      left = "calc(68% + 60px)",
-      right = 5,
-      class = "panel panel-default",
+          )
+      ),
       
-      tagList(
-        shiny::icon("flag", style = "color: gray"),
-        "Download country data"
-      ) %>%
-        div(class = "panel-heading",
-            style = "background-image:none;
-                  background: white;
-                  font-size: 16px; 
-                  font-weight: bold;
-                  padding: 15px !important;
-                  "),
-      
-      tagList(
-        shiny::icon("chart-pie",
-                    style = "color: gray"),
-        "Download setorial data"
-      ) %>%
-        div(class = "panel-body",
-            style = "background-image:none;
-                  background: white;
-                  font-size: 16px; 
-                  font-weight: bold;
-                  padding: 15px !important;
-                  ")
-    ),
-    
-    # Todos os gráficos
-    country_graphs,
-    
-    # Painel de distribuição setorial
-    absolutePanel(
-      top = "190px",
-      left = "calc(68% + 60px)",
-      right = "5px",
-      height = (top-200),
-
-      div(
+      # Painel de download
+      absolutePanel(
+        top = 70,
+        left = "calc(68% + 60px)",
+        right = 5,
         class = "panel panel-default",
-        style ="
+        
+        tags$table(
+          tags$tr(
+            tags$td(
+              shiny::icon("flag", style = "font-size: 32px; color: gray"),
+              rowspan = 2
+            ),
+            tags$td(
+              "Download country data",
+              style = "padding: 0px 15px;
+                      font-weight: bold;
+                      font-size: 16px", width = "100%")
+          ),
+          tags$tr(
+            tags$td(
+              tags$a("WIOD.13", href = "dowmload/WIOD.13/bra.xlsx"),
+              " | ",
+              tags$a("WIOD.16", href = "dowmload/WIOD.13/bra.xlsx"),
+              style = "text-align: center;
+                      font-size: 12px"
+            )
+          )
+        )
+        
+        %>%
+          div(class = "panel-heading",
+              style = "background-image:none;
+                  background: white;
+                  font-size: 16px; 
+                  padding: 5px 15px !important;
+                  "),
+        
+        tags$table(
+          tags$tr(
+            tags$td(
+              shiny::icon("chart-pie",
+                          style = "font-size: 32px; color: gray"),
+              rowspan = 2
+            ),
+            tags$td(
+              "Download setorial data",
+              style = "padding: 0px 15px;
+                      font-weight: bold;
+                      font-size: 16px", width = "100%")
+          ),
+          tags$tr(
+            tags$td(
+              tags$a("WIOD.13", href = "dowmload/WIOD.13/bra.xlsx"),
+              " | ",
+              tags$a("WIOD.16", href = "dowmload/WIOD.13/bra.xlsx"),
+              style = "text-align: center;
+                      font-size: 12px"
+            )
+          )
+        ) %>%
+          div(class = "panel-body",
+              style = "background-image:none;
+                  background: white;
+                  font-size: 16px; 
+                  padding: 5px 15px !important;
+                  ")
+      ),
+      
+      # Todos os gráficos
+      country_graphs,
+      
+      # Painel de distribuição setorial
+      absolutePanel(
+        top = "190px",
+        left = "calc(68% + 60px)",
+        right = "5px",
+        height = (top-200),
+        
+        div(
+          class = "panel panel-default",
+          style ="
                 position:sticky;
                 top:0;
                 height: calc(100vh - 120px);
               ",
-        textOutput("indicador") %>%
-          div(class = "panel-heading",
-              style = "background-image:none;
+          textOutput("indicador") %>%
+            div(class = "panel-heading",
+                style = "background-image:none;
                   background: white;
                   font-size:16px; 
                   font-weight: bold;
                   padding: 3px 5px;
                   "),
-        tagList(
-          p("Setorial data", style = "text-align: center; padding: 4px"),
-          tabsetPanel(
-            type = "tabs",
-            tabPanel(
-              "WIOD.13",
-              dataTableOutput("setores_pais_13")
-            ),
-            tabPanel(
-              "WIOD.16",
-              dataTableOutput("setores_pais_16")
+          tagList(
+            p("Setorial data", style = "text-align: center; padding: 4px"),
+            tabsetPanel(
+              type = "tabs",
+              tabPanel(
+                "WIOD.13",
+                dataTableOutput("setores_pais_13")
+              ),
+              tabPanel(
+                "WIOD.16",
+                dataTableOutput("setores_pais_16")
+              )
             )
-          )
-        ) %>%
-          div(class = "panel-body",
-              style = "padding:0;")
-      )
-    ),
-    
-  )
-),
-
-conditionalPanel(
-  "output.show_info_panel !=0",
+          ) %>%
+            div(class = "panel-body",
+                style = "padding:0;")
+        )
+      ),
+      
+    )
+  ),
   
-  absolutePanel(
-    id = "info_background",
-    style = "
+  conditionalPanel(
+    "output.show_info_panel !=0",
+    
+    absolutePanel(
+      id = "info_background",
+      style = "
           position: fixed !important;
           top: 45px;
           left: 0px;
@@ -180,23 +216,23 @@ conditionalPanel(
           text-align: center;
           z-index: 50000;
         "
-  ),
-  
-  absolutePanel(
-    top = "calc(50vh - 30vh)",
-    left = "calc(50vw - 30vw)",
-    width = "60vw",
-    height = "60vh",
-    class="panel panel-default",
-    style = "z-index: 50000;",
-    div(class = "panel-heading",
-        textOutput("info_indicator"),
-        actionLink(
-          "info_close_button",
-          label = NULL,
-          top = 5,
-          right = 5,
-          style = "
+    ),
+    
+    absolutePanel(
+      top = "calc(50vh - 30vh)",
+      left = "calc(50vw - 30vw)",
+      width = "60vw",
+      height = "60vh",
+      class="panel panel-default",
+      style = "z-index: 50000;",
+      div(class = "panel-heading",
+          textOutput("info_indicator"),
+          actionLink(
+            "info_close_button",
+            label = NULL,
+            top = 5,
+            right = 5,
+            style = "
               position: absolute;
               top: 5px;
               right: 10px;
@@ -204,14 +240,12 @@ conditionalPanel(
               font-size: 14px;
               color: gray;
             ",
-          icon = icon("times")
-        )
-    ),
-    
-    div(class = "panel-body",
-        uiOutput("info_text"))
-  ) 
+            icon = icon("times")
+          )
+      ),
+      
+      div(class = "panel-body",
+          uiOutput("info_text"))
+    ) 
+  )
 )
-)
-
-
