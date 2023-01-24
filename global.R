ffa10694551eadcd02b2bda210800c0572caa03e
Rodrigo Franklin <rodrigo.franklin.br@gmail.com>
2023-01-24 23:49:16 +0000

    Update global.R - default language

--- global.R
+++ global.R
@@ -31,7 +31,7 @@ panel_bgcolor <- "rgba(252,252,252,1)"
 ## Initial setup ####
 default_year <- 2007
 default_indicator <- "surplus_value.empe_p.r.pc"
-default_language <- colnames(language_file)[2]
+default_language <- "English"
 profile_indicators <- c("surplus_value.empe_p.r.pc",
                         "gdp.s.mv",
                         "gdp.s.us",
